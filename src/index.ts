c86d6420ce88360b68df5982adbdfbb09cff72ca
Elias Johansson <elias_johansson@hotmail.se>
2019-06-02 01:18:57 +0200

    Typeorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello from index.ts");
+import { createConnection, Connection } from "typeorm";
+import { User } from "./entity/User";
+
+const startServer = async () => {
+  const connection: Connection = await createConnection("development");
+
+  let user = new User();
+  user.firstName = "Elias";
+  user.lastName = "Johansson";
+
+  let userRepository = connection.getRepository(User);
+  userRepository.clear();
+  await userRepository.save(user);
+  console.log(await userRepository.find());
+};
+
+startServer();
