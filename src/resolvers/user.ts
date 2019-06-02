3019172a6f683d34e4883ab652a483bd2bf5d025
Elias Johansson <elias_johansson@hotmail.se>
2019-06-02 16:49:25 +0200

    Generated types for user resolvers

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,14 +1,19 @@
-import { IResolvers } from "graphql-tools";
-import { getRepository } from "typeorm";
+import { getRepository, In } from "typeorm";
 import { User } from "../entity/User";
+import { QueryResolvers } from "../generated/graphql";
 
-const users = async () => {
-  const userRepository = getRepository(User);
-  return userRepository.find();
-};
-
-export const resolvers: IResolvers = {
-  Query: {
-    users
+export const Query: QueryResolvers = {
+  users: (_, { ids }) => {
+    const userRepository = getRepository(User);
+    if (ids) {
+      return userRepository.find({ id: In(ids) });
+    }
+    return userRepository.find();
+  },
+  user: (_, { id }) => {
+    const userRepository = getRepository(User);
+    return userRepository.findOneOrFail(id);
   }
 };
+
+export const resolvers = { Query };
